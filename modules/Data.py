008d1c85f99a9ca764c772d14fbe5c03cb7e195a
Terrance Wong <twongzhw@gmail.com>
2025-07-26 11:26:18 -0700

    changed to google auth2.0 instead of service account for ease of use

--- modules/Data.py
+++ modules/Data.py
@@ -1,24 +1,58 @@
 import os
+import pickle
 from dotenv import load_dotenv
 import gspread
-from google.oauth2.service_account import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.errors import HttpError
+
 
 load_dotenv()
 
 class DataToSheet:
     def __init__(self):
-        creds = Credentials.from_service_account_file(
-            os.getenv("CREDENTIALS"),
-            scopes=[
+        try: 
+            SCOPES = [
                 "https://www.googleapis.com/auth/spreadsheets",
-                "https://www.googleapis.com/auth/drive"
+                'https://www.googleapis.com/auth/drive.metadata.readonly'
             ]
-        )
-        client = gspread.authorize(creds)
-        self.ws = client.open(os.getenv("SPREADSHEET")).worksheet(os.getenv("WORKSHEET"))
+            TOKEN_PATH = 'token.pickle'
 
+            creds  = InstalledAppFlow.from_client_secrets_file(
+                os.getenv('CREDENTIALS'),
+                SCOPES
+            ).run_local_server(port=0)
+
+            self.gc = gspread.authorize(creds)
+            try:
+                ss = self.gc.open_by_key(os.getenv('SPREADSHEET_ID'))
+                self.ws = ss.worksheet(os.getenv('WORKSHEET_NAME'))
+            except gspread.SpreadsheetNotFound:
+                print("Error: Can't find spreadsheet ID")
+                return
+            except gspread.WorksheetNotFound:
+                print("Error: Can't find Worksheet Name")
+            return
+        except HttpError as error: 
+            print(f"Error opening sheet: {error}")
+            return error
+
+    
     def addData(self, company, position, date, status):
-        self.ws.append_row([company, position, date, status])
+
+        try: 
+            all_val = self.ws.get_all_values()
+            next_row = len(all_val) + 1
+
+            cell_range = f"A{next_row}:D{next_row}"
+            self.ws.update(cell_range, 
+                    [[company, position, date, status]],
+                    value_input_option="USER_ENTERED")
+            print(f"Wrote to {cell_range}")
+        except HttpError as error:
+            print(f"Error occurred: {error}")
+            return error
+        
 
 
 
